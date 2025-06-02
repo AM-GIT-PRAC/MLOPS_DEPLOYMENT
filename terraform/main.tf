a7f0ceedab2101f49b8a368842fae7e9e0635437
AM-GIT-PRAC <makthala.a@outlook.com>
2025-06-02 16:41:41 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-#############################
-#       VPC CONFIG         #
-#############################
-
+# VPC Configuration
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   version = "5.19.0"
@@ -17,114 +14,29 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
   map_public_ip_on_launch = true
+
+  tags = {
+    Environment = "production"
+    Project     = "fraud-detection"
+  }
 }
 
-
-#############################
-#      IAM FOR EKS         #
-#############################
-
+# IAM Roles for EKS
 resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks_cluster_role"
-  assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume.json
+  name = "${var.cluster_name}-cluster-role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
-resource "aws_iam_role" "eks_node_role" {
-  name = "eks_node_role"
-  assume_role_policy = data.aws_iam_policy_document.eks_nodes_assume.json
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_node_AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_node_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_node_AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_node_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_node_AmazonEKSCNIPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_node_role.name
-}
-
-data "aws_iam_policy_document" "eks_cluster_assume" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-  }
-}
-
-data "aws_iam_policy_document" "eks_nodes_assume" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
-
-#############################
-#        EKS CLUSTER       #
-#############################
-
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = module.vpc.public_subnets
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy
-  ]
-}
-
-
-#############################
-#     EKS NODE GROUP       #
-#############################
-
-resource "aws_eks_node_group" "eks_nodes" {
-  cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = var.node_group_name
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = module.vpc.public_subnets
-
-  scaling_config {
-    desired_size = var.desired_size
-    max_size     = var.max_size
-    min_size     = var.min_size
-  }
-
-  instance_types = [var.instance_type]
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_node_AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.eks_node_AmazonEC2ContainerRegistryReadOnly,
-    aws_iam_role_policy_attachment.eks_node_AmazonEKSCNIPolicy
-  ]
-}
-
-
-#############################
-#         ECR              #
-#############################
-
-resource "aws_ecr_repository" "fraud_detection_repo" {
-  name = var.ecr_repo_name
-  image_tag_mutability = "MUTABLE"
-  force_delete = true
-}
+resource "aws_iam_role"
